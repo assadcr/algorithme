5f9699908c094f3f9d0a6101706bae862dd9e6ef
assadcr <assadoucs75020@gmail.com>
2024-02-13 16:59:45 +0100

    algo

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// un commentaire en javascript
+
+/*
+        un commentaire multilingue
+        en javascript
+
+
+/* --------------------------Affichage------------------- */
+// afficher des informations dans la co,sole de debbugage du navigateur
+console.log ('hello,world')
+/* --------------------------Les variablesl------------------- */
+// declarer d'une variable la declarer une fois
+let uneVariable;
+
+// affectation d'une variable
+uneVariable = ' Je suis une variable'
+
+console.log(uneVariable)
+// les 2 se font généralement en meme temps, les variables sont sensible a la case 
+let helloWorld = 'hello,World!';
+uneVariable = ' je suis une nouvelle variable!';
+
+
+console.log(helloWorld);
+
+ // les differents type de variables
+
+ let messages = 'un message'
+ let number = 20
+ let décimal = 20.5
+ let boolean = true;
+ let boolean2 = false;
+
+ // Les opérations arithétiques (+, -, *, /)
+ // declarer 3 variables sum, number,number2
+ //affecter le nombre 10 a number1
+ //affecter le nombre 20 a number 2
+ //affecter a sum l'operation de multiplication des 2 varaibles précédentes
+// afficher le resultat en console
+ let sum
+ let number1 = 10
+ let number2 = 20
+ sum = number1 * number2
+ console.log(sun);
+/*----------------------(------------------la concatenation------------------------------------------------ */
+
+let firstName = 'Assa'
+let lastName = 'DOUCOURE'
+
+// je m'appelle Firstname
+
+console.log("Je m'appelle " + firstName + ' ' + lastName);
+
+// déclarer une variable city , afficher le message j'adore vivre a Paris
+
+let city = 'PARIS'
+city = "PARIS"
+city = "PARIS"
+
+console.log ("j'adore vivre a " + city)
+// modifier le message pour afficher j'adore vivre a <city> et je m'appelle
+<firstName> <lastName></lastName>
+console.log('j'adore vivre a paris  ${city} .)
+console.log ( j'adore vivre a $ (city) et je m'appelle $(firstName ${lastName})
