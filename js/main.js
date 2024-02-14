ba864ddda9d3eec6d4fcf08103a6890c8326e81b
assadcr <assadoucs75020@gmail.com>
2024-02-14 16:57:19 +0100

    devoir du jour

--- js/main.js
+++ js/main.js
@@ -7,8 +7,11 @@
 
 /* --------------------------Affichage------------------- */
 // afficher des informations dans la co,sole de debbugage du navigateur
-console.log ('hello,world')
-/* --------------------------Les variablesl------------------- */
+console.log ('hello,world');
+// afficher une boite de dialogue avec champ a remplir
+//let reponse = prompt('quel est votre age?');
+//console.log (response);
+/* --------------------------Les variables------------------- */
 // declarer d'une variable la declarer une fois
 let uneVariable;
 
@@ -41,7 +44,7 @@ console.log(helloWorld);
  let number1 = 10
  let number2 = 20
  sum = number1 * number2
- console.log(sun);
+ //console.log(sun);
 /*----------------------(------------------la concatenation------------------------------------------------ */
 
 let firstName = 'Assa'
@@ -59,6 +62,89 @@ city = "PARIS"
 
 console.log ("j'adore vivre a " + city)
 // modifier le message pour afficher j'adore vivre a <city> et je m'appelle
-<firstName> <lastName></lastName>
-console.log('j'adore vivre a paris  ${city} .)
-console.log ( j'adore vivre a $ (city) et je m'appelle $(firstName ${lastName})
+{/* <firstName> <lastName></lastName> */}
+
+
+//------------------------------------  informationsnstructions conditionnelles------------- */
+
+// demander a l'utulisateur s'il accepte les cgv condition général de vente
+
+
+//let agree = prompt ("Accepter-vous les cgv?");
+// si ,sinon ( if,else)
+       //if (agree =='ok') {
+        //console.log ('vous avez accepter les conditions');
+      //  } else {
+                console.log (" vous n'avez pâs accepter les conditions!");
+       // }
+        // si,si non si ,sinon
+        // si il repond yes//vous etes redirigé vers le site en anglais
+        //sinon si il repond oui
+        //vous etes redirigé vers le site en francais
+        //sinon si il repond dans// vous etes redirigéé vers le site en russe
+        //sinon
+        // vous etes rédigigé vers le site en russe
+        // vous ne pouvez acceder au site
+       // if(agree == 'yes'){
+        console.log("vous etes redigérer sur le site en anglais");
+
+        //}else if( agree == "oui"  ){
+        console.log ("vous etes redirigé vers le site en francais");
+      // }else if (agree == 'da'){
+        console.log ( 'vous etes redirigé vers le site en russe');
+
+       //}else{
+        console.log ('vous ne pouvez acceder au site');
+      // }
+//console.log ( "la suite...");
+
+
+ //------------------- les operateurs de comparaison-----------------*/
+ // ==,>;<=;<},!=,===
+ // déclarer une variable qui s'appelle variable lui assigner la valeur 16
+ // si la variable est égale a 16 afficher felicitations
+
+ //let variable = 16
+ //if (variable == 16)//{
+        console.log ('droit de conduire');          
+ //}
+ //let age = prompt('quel est ton age');
+ //let pays = prompt ('quelle est votre nationalité');
+
+ //if(
+        //nationalté =='francais'&& age >= 18 ||
+       // nationalité != 'francais'
+       // ) {
+       
+       
+       
+        //console.log ('vous avez le droit a une assitance juridique');
+ //}
+let nationalité = prompt ('quelle est votre nationalité');
+console.log (nationalité);
+  //if (nat == 'francais') {
+     let age = prompt ('quel est votre age?');
+     if (age >=18) {
+        console.log ('vous avez droit aux APL');
+     }   
+//} else {
+        console.log ("vous n'avez aucun droit");
+
+
+//}
+
+
+/*--------------------les boucles-------------------- */
+
+// les boucles sont des structures permettant d'excuter plusieurs fois des instructions
+// boucle a itération définies (while)
+let i =0
+while (1 <10){
+        let res= i * 7;
+   console.log (' $(i)*7 = ${res}');
+   i = i + i;
+}
+
+// afficher la table de multiplication de 7
+
+ 
